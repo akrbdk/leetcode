d489c2066b3b90a4a1d67140b41bcf079da067a1
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-07-29 17:38:04 +0300

    Time: 0 ms (100.00%), Space: 20 MB (7.00%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+    function isSubsequence($s, $t) {
+        $sArray = str_split($s);
+        $tArray = str_split($t);
+        
+        for($i=0; $i<count($sArray); $i++){
+            for($j=0;$j<count($tArray); $j++){
+                if($sArray[$i] == $tArray[$j]){
+                    $hitCount++;
+                    $i++;
+                }
+            }
+            $j++;
+            
+        }
+        
+        if(count(array_filter($sArray))==0){
+            $hitCount = count($sArray);
+        }
+        
+        return  ($hitCount==count($sArray));
+    }
+}
