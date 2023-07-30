8bb2121beee529e9d03b8433435b83e3366a0911
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-07-30 11:47:49 +0300

    Time: 10 ms (92.31%), Space: 19.3 MB (28.85%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.php
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.php
@@ -8,6 +8,29 @@ class Solution {
         $evenCnt = 0;
         
         foreach($nums as $num){
+            if($this->dNum($num) % 2 === 0){
+                $evenCnt++;;
+            }
+        }
+        
+        return $evenCnt;
+    }
+    
+    function dNum($num) {
+        $cnt = 0;
+        
+        while($num >= 1){
+            $num = $num / 10;
+            $cnt++;
+        }
+        
+        return $cnt;
+    }
+    
+    function findNumbers2($nums) {
+        $evenCnt = 0;
+        
+        foreach($nums as $num){
             if(strlen($num) % 2 === 0){
                 $evenCnt++;;
             }
