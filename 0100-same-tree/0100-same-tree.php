79f1823152973915a0786bf8bb4066ce4e7cc336
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-08-04 22:21:38 +0300

    Time: 5 ms (68.29%), Space: 19.1 MB (59.76%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.php
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q) {
+        if(empty($p) && empty($q)){
+            return true;
+        }
+        
+        if(empty($p) || empty($q)){
+            return false;
+        }
+        
+        if($p->val == $q->val && $this->isSameTree($p->left, $q->left) && $this->isSameTree($p->right, $q->right)){
+            return true;
+        }
+        
+        return false;
+    }
+}
