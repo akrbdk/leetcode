ee1bc912e4ed7fd9fb9c2fce47bf56ed19472049
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-08-23 15:49:25 +0300

    Time: 0 ms (100.00%), Space: 19.1 MB (40.00%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.php
@@ -0,0 +1,28 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return Integer
+     */
+    function getDecimalValue($head) {
+        $ans = 0;
+        
+        while($head){
+            $ans = $ans * 2 + $head->val;
+            $head = $head->next;
+        }
+        
+        return $ans;
+    }
+}
