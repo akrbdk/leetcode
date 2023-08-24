7c56f4c89e5660dc86f3d1b7ddfd528d8e444b14
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-08-24 12:18:45 +0300

    Time: 0 ms (100.00%), Space: 19.2 MB (15.52%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.php
+++ 0191-number-of-1-bits/0191-number-of-1-bits.php
@@ -4,6 +4,17 @@ class Solution {
      * @return Integer
      */
     function hammingWeight($n) {
+        $str = decbin($n);
+        $str = str_replace(0,'',$str);
+    
+    return strlen($str);
+    }
+    
+    function hammingWeight2($n) {
+        
+    }
+    
+    function hammingWeight3($n) {
         $count = 0;
 
         for($i=0; $i < 32; $i++){
