6a84981acf3974ddaa334d221b0f8bdea3cdada1
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-08-26 16:27:24 +0300

    Time: 14 ms (23.20%), Space: 20.7 MB (85.64%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.php
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxDepth($root) {
+        if(!$root){
+            return 0;
+        }
+        
+        $left = $this->maxDepth($root->left);
+        $right = $this->maxDepth($root->right);
+        
+        print_r([$left, $right]);
+        
+        return max($left, $right) + 1;
+    }
+}
