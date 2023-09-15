2398d9f3695acd6270f01370c1a366705c7ffd62
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-09-15 21:42:18 +0300

    Time: 123 ms (95.65%), Space: 20.5 MB (60.00%) - LeetHub

--- 0055-jump-game/0055-jump-game.php
+++ 0055-jump-game/0055-jump-game.php
@@ -10,14 +10,23 @@ class Solution {
             return true;
         }
         
+        //This line sets the initial target position to the last index of the array. 
+        //The goal is to reach this position.
         $target = $numsCnt - 1;
         
+        //This is a for loop that starts from the second-to-last element of the array and iterates backwards.
         for($i = $numsCnt - 2; $i >= 0; $i--){
+            //This calculates the distance from the current position ($i) to the target position.
             $distance = $target - $i;
-            
+                    
+            //If the jump distance from the current position is greater than or equal to the calculated 
+            //distance to the target, then it means we can jump to the target position. Therefore, we update 
+            //the target position to be the current position ($i).
             if($nums[$i] >= $distance){
                 $target = $i;
-            }elseif($i === 0){
+            }elseif($i === 0){ 
+                //If we've reached the first position and haven't been able to reach the target, 
+                //it means it's not possible to reach the end. In this case, the function returns false.
                 return false;
             }
         }
