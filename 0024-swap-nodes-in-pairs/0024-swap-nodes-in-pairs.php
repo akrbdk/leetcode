edcb0cabe2d4025b5be0a9a29e093c08158f2c19
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-09-17 13:10:02 +0300

    Time: 4 ms (68.00%), Space: 19.2 MB (48.00%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.php
@@ -0,0 +1,38 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function swapPairs($head) {
+        $dummy = new ListNode();
+        $dummy->next = $head;
+        
+        $prev = $dummy;
+        $cur = $dummy->next;
+        
+        while($cur && $cur->next){
+            $next = $cur->next;
+            
+            $prev->next = $next;
+            $cur->next = $next->next;
+            $next->next = $cur;
+            
+            $prev = $cur;
+            $cur = $cur->next;
+        }
+        
+        return $dummy->next;
+    }
+}
