bce38af71715a42d4622958cd0c50f393c3bb4fe
Alexander Karabudka <sanyakarabudka@gmail.com>
2023-12-20 23:29:03 +0200

    Time: 0 ms (100.00%), Space: 2.2 MB (62.43%) - LeetHub

---
+++ 0139-word-break/0139-word-break.go
@@ -0,0 +1,23 @@
+func wordBreak(s string, wordDict []string) bool {
+    dp := make([]bool, len(s)+1)
+    dp[0] = true
+    dict := make(map[string]struct{})
+    
+    for _, val := range wordDict {
+        dict[val] = struct{}{}
+    }
+    
+    for i := 1; i <= len(s); i++ {
+        for j := 0; j < i; j++ {
+            if dp[j] {
+                substr := s[j:i]
+                if _, ok := dict[substr]; ok {
+                    dp[i] = true
+                    break
+                }
+            }
+        }
+    }
+    
+    return dp[len(dp)-1]
+}
