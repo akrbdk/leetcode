6d92ceda016de2b3839b3b6cb86c3097585a3230
Alexander Karabudka <sanyakarabudka@gmail.com>
2024-05-26 23:39:26 +0300

    Time: 31 ms (56.14%), Space: 19.9 MB (85.19%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.php
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.php
@@ -6,15 +6,18 @@ class Solution {
      * @return Float
      */
     function findMedianSortedArrays($nums1, $nums2) {
-        if (empty($nums1) && empty($nums2)) {
-    		return (float)0;
-    	}
+        if(empty($nums1) && empty($nums2)) {
+            return (float)0;
+        }
+        
         $nums = array_merge($nums1, $nums2);
         sort($nums);
-        if (count($nums) % 2 !=0) {
-        	return $nums[count($nums) / 2 ];
-        } else {
-        	return  ($nums[count($nums) / 2 ]+ $nums[count($nums) / 2 -1]) / 2;
+        $numsCnt = count($nums) / 2;
+        
+        if(count($nums) % 2 === 0){
+            return ($nums[$numsCnt] + $nums[$numsCnt - 1]) / 2;
         }
+        
+        return $nums[$numsCnt];
     }
 }
