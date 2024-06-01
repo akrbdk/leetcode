20071b300116e93d56d2cdc06bf56b550d3eb096
Alexander Karabudka <sanyakarabudka@gmail.com>
2024-06-02 00:35:46 +0300

    Time: 4 ms (74.48%), Space: 3.4 MB (64.62%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.go
@@ -0,0 +1,15 @@
+func intToRoman(num int) string {
+    vals := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+    syms := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+    
+    res := ""
+    
+    for i := 0; i < len(vals); i++ {
+        for num >= vals[i] {
+            res += syms[i]
+            num -= vals[i]
+        }
+    }
+    
+    return res
+}
