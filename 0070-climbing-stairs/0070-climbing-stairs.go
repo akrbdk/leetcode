51a3adf454ec57c915b47d9de5fa311d6be8b628
Alex Karabudka <sanyakarabudka@gmail.com>
2024-06-16 15:16:37 +0300

    Time: 1 ms (76.98%), Space: 2.1 MB (54.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.go
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -1,22 +1,8 @@
-class Solution {
-
-    /**
-     * @param Integer $n
-     * @return Integer
-     */
-    function climbStairs($n) {
-        if($n < 2) {
-            return $n;
-        }
-        
-        $dp = [];
-        $dp[1] = 1;
-        $dp[2] = 2;
-        
-        for($i = 3; $i <= $n; $i++){
-            $dp[$i] = $dp[$i - 1] + $dp[$i - 2];
-        }
-        
-        return $dp[$n];
+func climbStairs(n int) int {
+    first, second := 1, 1
+    for ; n > 1; n-- {
+        first, second = second, first + second
     }
+    
+    return second
 }
