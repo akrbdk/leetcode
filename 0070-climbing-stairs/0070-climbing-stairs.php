343ec0691388fd1c07d1cc0a8cba85c541d5c7c9
Alex Karabudka <sanyakarabudka@gmail.com>
2024-06-16 15:11:25 +0300

    Time: 0 ms (100.00%), Space: 20 MB (46.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.php
@@ -0,0 +1,22 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n) {
+        if($n < 2) {
+            return $n;
+        }
+        
+        $dp = [];
+        $dp[1] = 1;
+        $dp[2] = 2;
+        
+        for($i = 3; $i <= $n; $i++){
+            $dp[$i] = $dp[$i - 1] + $dp[$i - 2];
+        }
+        
+        return $dp[$n];
+    }
+}
