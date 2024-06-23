f06c86608ba4d4c00a377e23ad2346f5d74ca5a0
Alex Karabudka <sanyakarabudka@gmail.com>
2024-06-23 11:34:09 +0200

    Time: 7 ms (95.18%), Space: 6.4 MB (26.35%) - LeetHub

---
+++ 0136-single-number/0136-single-number.go
@@ -0,0 +1,13 @@
+func singleNumber(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    
+    ans := nums[0]
+    
+    for i := 1; i < len(nums); i++ {
+        ans = ans ^ nums[i]
+    }
+    
+    return ans
+}
