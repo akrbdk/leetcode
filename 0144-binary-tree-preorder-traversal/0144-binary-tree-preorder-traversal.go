39c783ff5a533fab91132b5019d60a414d21b193
Alex Karabudka <sanyakarabudka@gmail.com>
2024-06-24 22:19:23 +0200

    Time: 1 ms (76.75%), Space: 2.3 MB (9.26%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
@@ -7,11 +7,42 @@
  * }
  */
 
+//USE Stacks
+
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+    
+    ans := []int{}
+    stack := []*TreeNode{}
+    dummy := root
+    
+    for len(stack) != 0 || dummy != nil {
+        for dummy != nil {
+            stack = append(stack, dummy)
+            ans = append(ans, dummy.Val)
+            dummy = dummy.Left
+        }
+        
+        dummy = stack[len(stack) - 1].Right
+        
+        //The top node is then removed from the stack.
+        stack = stack[:len(stack) - 1]
+    }
+    
+    return ans
+}
+
+
+
+//DFS
+
 type Result struct {
     Ans []int
 }
 
-func preorderTraversal(root *TreeNode) []int {
+func preorderTraversal1(root *TreeNode) []int {
     res := Result{}
     res.dfs(root)
     return res.Ans
