f15cfab7b5af0e2cf3faead43850424228dab63a
Alex Karabudka <sanyakarabudka@gmail.com>
2024-06-26 12:11:28 +0200

    Time: 18 ms (72.85%), Space: 7 MB (17.50%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,4 +1,21 @@
 func majorityElement(nums []int) int {
+    cnt, ans := 0, 0
+    for _, num := range nums {
+        if cnt == 0 {
+            ans = num
+        }
+        
+        if num == ans {
+            cnt++
+        } else {
+            cnt --
+        }
+    }
+    
+    return ans
+}
+
+func majorityElement2(nums []int) int {
     res, times := nums[0], 1
     for i := 1; i < len(nums); i++ {
         if nums[i] == res {
