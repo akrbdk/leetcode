eaec3343b715a3998ce6ac4ddf45d3736527fc49
Alex Karabudka <sanyakarabudka@gmail.com>
2024-07-07 11:24:40 +0200

    Time: 23 ms (6.14%), Space: 8 MB (5.95%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    
+    left, right := countNodes(root.Left), countNodes(root.Right)
+    
+    return 1 + left + right
+}
