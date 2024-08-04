fb069d2dfcc6d41ff3759a8c36b3a494766d94c3
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-04 14:58:10 +0200

    Time: 5 ms (85.71%), Space: 20 MB (14.29%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.php
@@ -0,0 +1,14 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function canWinNim($n) {
+        if ($n % 4 == 0) {
+            return false;
+        }
+        
+        return true;
+    }
+}
