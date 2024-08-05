50dec1c23f85462e272028c8f35c81de4e9221eb
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-05 12:12:38 +0200

    Time: 42 ms (58.54%), Space: 19.9 MB (63.41%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isPowerOfThree($n) {
+        if($n <= 0) {
+            return false;
+        }
+        
+        while($n % 3 == 0) {
+            $n /= 3;
+        }
+        
+        return $n === 1;
+    }
+}
