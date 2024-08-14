1f35010bd73a9e7b900c9e032b95ef2cd17ee209
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-14 15:52:52 +0200

    Time: 485 ms (8.96%), Space: 191.6 MB (5.47%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.go
@@ -0,0 +1,24 @@
+func repeatedSubstringPattern(s string) bool {
+    sLen := len(s)
+    
+    for i := (sLen / 2); i >= 1; i-- {
+        if sLen % i != 0 {
+            continue
+        }
+        
+        sub := s[:i]
+        var tmpS string
+        
+        for j := 0; j < (sLen / len(sub)); j++ {
+            tmpS += sub
+        }
+        
+        fmt.Println(tmpS)
+                        
+        if tmpS == s {
+            return true
+        }
+    }
+    
+    return false
+}
