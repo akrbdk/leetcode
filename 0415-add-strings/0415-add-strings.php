0a67a6674fb1575f74c3e3e49bbd96e4554b6cfd
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-18 11:14:16 +0200

    Time: 33 ms (5.26%), Space: 20 MB (36.84%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.php
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * @param String $num1
+     * @param String $num2
+     * @return String
+     */
+    function addStrings($num1, $num2) {
+        $n1Len = strlen($num1) - 1;
+        $n2Len = strlen($num2) - 1;
+        $carry = 0;
+        $ans = '';
+        
+        while($n1Len >= 0 || $n2Len >= 0 || $carry > 0){
+            $n1Val = $n1Len >= 0 ? $num1[$n1Len] - '0' : 0;
+            $n2Val = $n2Len >= 0 ? $num2[$n2Len] - '0' : 0;
+            
+            $tmpSum = $n1Val + $n2Val + $carry;            
+            $carry = intdiv($tmpSum, 10);
+            $ans = ($tmpSum % 10) . $ans;
+
+            
+            $n1Len--;
+            $n2Len--;
+        }
+        
+        return $ans;
+    }
+}
