c0d01e0ea967caea696b566cd44a21b2b3627d5a
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-20 13:31:52 +0200

    Time: 42 ms (78.00%), Space: 6.9 MB (93.00%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.go
@@ -0,0 +1,33 @@
+func islandPerimeter(grid [][]int) int {
+    rows := len(grid)
+    cols := len(grid[0])
+    ans := 0
+    
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            if grid[i][j] == 1 {
+                //top border 
+                if i == 0 || grid[i - 1][j] == 0 {
+                    ans++
+                }
+                
+                //bottom border
+                if i == (rows - 1) || grid[i + 1][j] == 0 {
+                    ans++
+                }
+                
+                //left border
+                if j == 0 || grid[i][j - 1] == 0 {
+                    ans++
+                }
+                
+                //right border
+                if j == (cols - 1) || grid[i][j + 1] == 0 {
+                    ans++
+                }
+            }
+        }
+    }
+    
+    return ans
+}
