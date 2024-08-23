dbb071b45acb466367eab4e42ca4d83734d521fe
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-23 17:33:33 +0200

    Time: 83 ms (6.67%), Space: 21.2 MB (60.00%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.php
+++ 0495-teemo-attacking/0495-teemo-attacking.php
@@ -11,11 +11,12 @@ class Solution {
         for($i = 0; $i < count($timeSeries); $i++){
             $end = $timeSeries[$i] + $duration;
             
-            if(isset($timeSeries[$i + 1])){
-                $end = $timeSeries[$i + 1] - $timeSeries[$i];
+            if(isset($timeSeries[$i + 1]) && $timeSeries[$i + 1] < $end){
+                $end = $timeSeries[$i + 1];
             }
             
-            $ans += $end < $duration ? $end : $duration;
+            $range = $end - $timeSeries[$i];
+            $ans += $range < $duration ? $range : $duration;
         }
         
         return $ans;
