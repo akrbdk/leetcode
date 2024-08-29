9c3984fcaae2bdf5dbc367f0a7bc96525b56e840
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-29 14:19:10 +0200

    Time: 0 ms (100.00%), Space: 20 MB (60.00%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.php
@@ -0,0 +1,15 @@
+class Solution {
+
+    /**
+     * @param String $a
+     * @param String $b
+     * @return Integer
+     */
+    function findLUSlength($a, $b) {
+        if($a === $b){
+            return -1;
+        }
+        
+        return max(strlen($a), strlen($b));
+    }
+}
