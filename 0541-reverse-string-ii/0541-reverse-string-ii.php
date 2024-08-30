ba2f94bd5f1a7822aead64d9b1b7a9f68ad7c15c
Alex Karabudka <sanyakarabudka@gmail.com>
2024-08-30 11:11:25 +0200

    Time: 7 ms (75.00%), Space: 20 MB (81.82%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.php
+++ 0541-reverse-string-ii/0541-reverse-string-ii.php
@@ -6,34 +6,17 @@ class Solution {
      * @return String
      */
     function reverseStr($s, $k) {
-        $sLen = strlen($s);
-        
-        /*if($sLen < $k){
-            $r = $sLen - 1;
-            $l = 0;
-            while($l <= $r){
-                $tmp = $s[$l];
-                $s[$l] = $s[$r];
-                $s[$r] = $tmp;
-                $l++;
-                $r--;
-            }
-            return $s;
-        }*/
-        
-        
-        
-        for($i = 0; $i < $sLen; $i += 2 * $k){
+        $sLen = strlen($s) - 1;
+    
+        for($i = 0; $i <= $sLen; $i += 2 * $k){
         
             $l = $i;
             $r = $i + $k - 1;
             
-            if($r > ($sLen - 1)){
-                $r = $sLen - 1;
+            if($r > $sLen){
+                $r = $sLen;
             }
-            
-            print_r([$i, $l, $r, $s[$l], $s[$r]]);
-            
+                        
             while($l <= $r){
                 $tmp = $s[$l];
                 $s[$l] = $s[$r];
