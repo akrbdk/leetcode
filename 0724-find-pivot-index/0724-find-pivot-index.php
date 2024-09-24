3a8601c9e2160778ed2669ff984c4fc24ee5769a
Alex Karabudka <sanyakarabudka@gmail.com>
2024-09-24 10:27:17 +0200

    Time: 3553 ms (5.88%), Space: 21 MB (86.76%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function pivotIndex($nums) {
+        $numsCnt = count($nums);
+        $lSum = 0;
+        
+        for($i = 0; $i < $numsCnt; $i++){
+            $rSum = 0;
+            for($j = $i + 1; $j < $numsCnt; $j++){
+                $rSum += $nums[$j];
+            }
+            
+            print_r([$lSum, $rSum]);
+            
+            if($lSum == $rSum){
+                return $i;
+            }
+            
+            $lSum += $nums[$i];
+        }
+        
+        return -1;
+    }
+}
